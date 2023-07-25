e16f083aecfe4076006c6a2160f4f652c53828e7
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-25 18:20:50 +0300

    Added two funcs doHTTP to do requests and GetRequestToken to get token from API

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/m0rk0vka/go-pocket-sdk
+
+go 1.20
+
+require github.com/pkg/errors v0.9.1 // indirect
--- pocket.go
+++ pocket.go
@@ -1,11 +1,31 @@
 package pocket
 
 import (
-	"errors"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"strings"
+	"time"
+
+	"github.com/pkg/errors"
 )
 
-const ()
+const (
+	host         = "https://getpocket.com/v3"
+	authorizeURL = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s"
+
+	endpointAdd          = "/add"
+	endpointRequestToken = "/oauth/request"
+	endpointAuthorize    = "/oauth/authorize"
+
+	xErrorHeader = "X-Error"
+
+	defaultTimeout = 5 * time.Second
+)
 
 type (
 	requestTokenRequest struct {
@@ -59,4 +79,79 @@ func (i AddInput) generateRequest(consumerKey string) addRequest {
 		AccessToken: i.AccessToken,
 		ConsumerKey: consumerKey,
 	}
+}
+
+// Client as a getpocket API client
+type Client struct {
+	client      *http.Client
+	consumerKey string
+}
+
+func NewClient(consumerKey string) (*Client, error) {
+	if consumerKey == "" {
+		return nil, errors.New("consumer key is empty")
+	}
+
+	return &Client{
+		client: &http.Client{
+			Timeout: defaultTimeout,
+		},
+		consumerKey: consumerKey,
+	}, nil
+}
+
+func (c *Client) doHTTP(ctx context.Context, endpoint string, body interface{}) (url.Values, error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return url.Values{}, errors.WithMessage(err, "failed to marshal input body")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, host+endpoint, bytes.NewBuffer(b))
+	if err != nil {
+		return url.Values{}, errors.WithMessage(err, "failed to create new request")
+	}
+
+	req.Header.Set("Content-Type", "application/json; charset=UTF8")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return url.Values{}, errors.WithMessage(err, "failed to send http request")
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		err := fmt.Sprintf("API Error: %s", resp.Header.Get(xErrorHeader))
+		return url.Values{}, errors.New(err)
+	}
+
+	respB, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return url.Values{}, errors.WithMessage(err, "failed to read request body")
+	}
+
+	values, err := url.ParseQuery(string(respB))
+	if err != nil {
+		return url.Values{}, errors.WithMessage(err, "failed to parse response body")
+	}
+
+	return values, nil
+}
+
+func (c *Client) getRequestToken(ctx context.Context, redirectUrl string) (string, error) {
+	inp := &requestTokenRequest{
+		ConsumerKey: c.consumerKey,
+		RedirectURI: redirectUrl,
+	}
+
+	values, err := c.doHTTP(ctx, endpointRequestToken, inp)
+	if err != nil {
+		return "", err
+	}
+
+	if values.Get("code") == "" {
+		return "", errors.New("empty request token in API response")
+	}
+
+	return values.Get("code"), nil
 }
