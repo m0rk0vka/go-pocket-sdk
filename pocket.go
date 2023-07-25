968632ea8b5f08c91203cd07d2bd8c0623c1709a
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-25 16:14:58 +0300

    Added structs to work with pocket API

---
+++ pocket.go
@@ -0,0 +1,35 @@
+package pocket
+
+const ()
+
+type (
+	requestTokenRequest struct {
+		ConsumerKey string `json:"consumer_key"`
+		RedirectURI string `json:"redirect_uri"`
+	}
+
+	authorizeRequest struct {
+		ConsumerKey string `json:"consumer_key"`
+		Code        string `json:"code"`
+	}
+
+	AuthorizeResponse struct {
+		AccessToken string `json:"access_token"`
+		Username    string `json:"access_token"`
+	}
+
+	addRequest struct {
+		URL         string `json:"url"`
+		Title       string `json:"title,omitempty"`
+		Tags        string `json:"tags,omitempty"`
+		AccessToken string `json:"access_token"`
+		ConsumerKey string `json:"consumer_key"`
+	}
+
+	AddInput struct {
+		URL         string
+		Title       string
+		Tags        []string
+		AccessToken string
+	}
+)
