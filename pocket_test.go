6d4d5bb3bf84ff7d9b088b6a30c64fd86cce23e6
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-26 23:17:40 +0300

    Added unit-tests for get request token function

--- pocket_test.go
+++ pocket_test.go
@@ -1,6 +1,7 @@
 package pocket
 
 import (
+	"context"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -30,4 +31,75 @@ func newClient(t *testing.T, statusCode int, path, body string) *Client {
 		},
 		consumerKey: "key",
 	}
+}
+
+func TestClient_GetRequestToken(t *testing.T) {
+	type args struct {
+		ctx         context.Context
+		redirectUrl string
+	}
+
+	tests := []struct {
+		name                 string
+		args                 args
+		expectedStatusCode   int
+		expectedResponse     string
+		expectedErrorMessage string
+		want                 string
+		wantErr              bool
+	}{
+		{
+			name: "Ok",
+			args: args{
+				ctx:         context.Background(),
+				redirectUrl: "http://localhost",
+			},
+			expectedStatusCode: 200,
+			expectedResponse:   "code=qwe-rty-123",
+			want:               "qwe-rty-123",
+			wantErr:            false,
+		},
+		{
+			name: "Empty redirect URL",
+			args: args{
+				ctx:         context.Background(),
+				redirectUrl: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Empty response code",
+			args: args{
+				ctx:         context.Background(),
+				redirectUrl: "http://localhost",
+			},
+			expectedStatusCode: 200,
+			expectedResponse:   "code=",
+			wantErr:            true,
+		},
+		{
+			name: "Non-2XX Response",
+			args: args{
+				ctx:         context.Background(),
+				redirectUrl: "http://localhost",
+			},
+			expectedStatusCode: 400,
+			wantErr:            true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := newClient(t, tt.expectedStatusCode, "/v3/oauth/request", tt.expectedResponse)
+
+			got, err := c.getRequestToken(tt.args.ctx, tt.args.redirectUrl)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+
 }
