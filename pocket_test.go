7a5e50df7df916cdba488bb75e730c4ace52dc4e
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-26 23:33:15 +0300

    Added test for get authorization url function

--- pocket_test.go
+++ pocket_test.go
@@ -2,6 +2,7 @@ package pocket
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -102,4 +103,60 @@ func TestClient_GetRequestToken(t *testing.T) {
 		})
 	}
 
+}
+
+func TestClient_GetAuthorizationURL(t *testing.T) {
+	type args struct {
+		requestToken string
+		redirectUrl  string
+	}
+
+	want := func(args args) string {
+		return fmt.Sprintf("https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s", args.requestToken, args.redirectUrl)
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			args: args{
+				requestToken: "qwe-rty-123",
+				redirectUrl:  "http://localhost:80/",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Empty token",
+			args: args{
+				requestToken: "",
+				redirectUrl:  "http://localhost:80/",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Empty URL",
+			args: args{
+				requestToken: "qwe-rty-123",
+				redirectUrl:  "",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{}
+			got, err := c.GetAuthorizationURL(tt.args.requestToken, tt.args.redirectUrl)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, want(tt.args), got)
+			}
+		})
+	}
 }
